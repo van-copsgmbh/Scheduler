60219b8690d1d82d6101ae7968f3554f41a625e7
mkw <m.kwiatkowski@copsgmbh.com>
2024-08-23 09:21:53 +0200

    generic periodic job

--- Corima.Scheduler.Shared/PeriodicJob.cs
+++ Corima.Scheduler.Shared/PeriodicJob.cs
@@ -1,15 +1,52 @@
 ﻿using Quartz;
 using System;
+using System.Reflection;
 using System.Threading.Tasks;
 
 namespace Corima.Scheduler.Shared
 {
-    public class PeriodicJob : CorimaJob, ICorimaAutoJob
+    public abstract class RepeatedJob : CorimaJob, ICorimaAutoJob
     {
-        public JobKey Key { get; } = new JobKey("PeriodicJob", "Group1");
-        public ITrigger Trigger { get; } = new Triggers().RepeatedTrigger("PeriodicJob" + "Trigger", 60);
+        public JobKey Key { get; }
+        public ITrigger Trigger { get; }
 
+        protected RepeatedJob(TimeSpan fromSeconds)
+        {
+            this.Trigger = new Triggers().RepeatedTrigger(this.JobName + "Trigger", (int)fromSeconds.TotalSeconds);
+            this.Key = new JobKey(this.JobName, "Group1");
+        }
+    }
+
+
+    public class PeriodicJob2 : RepeatedJob
+    {
+        private static int counter = 1;
+
+        public PeriodicJob2() : base(TimeSpan.FromHours(2))
+        {
+        }
+
+        protected override string JobName { get; } = "PeriodicJob2";
+
+        protected override Task OnError(IJobExecutionContext context, Exception exception)
+        {
+            return Task.CompletedTask;
+        }
+
+        protected override async Task DoWork(IJobExecutionContext context)
+        {
+            Console.WriteLine($"PeriodicJob executed {counter++}");
+        }
+    }
+
+    public class PeriodicJob : RepeatedJob, ICorimaAutoJob
+    {
         protected override string JobName => "PeriodicJob";
+
+        public PeriodicJob() : base(TimeSpan.FromSeconds(50))
+        {
+            
+        }
         private static int counter = 1;
 
         protected override Task OnError(IJobExecutionContext context, Exception exception)
